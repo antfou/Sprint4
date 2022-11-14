923b546ad48a0007c348db8b3be52751231c4225
antfou <fougneranton@gmail.com>
2022-11-14 15:02:43 +0100

    Cleaned up code.

--- src/Uppgift15/GenericQueueClass.java
+++ src/Uppgift15/GenericQueueClass.java
@@ -1,18 +1,14 @@
 package Uppgift15;
 
-public class GenericQueueClass /*implements GenericQueueInterface<T>*/ {
-
-   /* public void put(T t) {
+public class GenericQueueClass<T> implements GenericQueueInterface<T> {
+    GenericQueueClass(){};
+   public void put(T t) {
 
     }
-
     public T take() {
         return null;
     }
-
     public int size() {
         return 0;
     }
-
-    */
 }
