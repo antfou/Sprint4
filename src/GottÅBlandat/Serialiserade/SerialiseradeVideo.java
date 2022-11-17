f00f93ba4ffdae81ea14ed4accf9e41d03f3f4a1
Anton Fougner <fougneranton@gmail.com>
2022-11-17 09:21:30 +0100

    Notes from Serializable lecture.

--- src/GottÅBlandat/Serialiserade/SerialiseradeVideo.java
+++ src/GottÅBlandat/Serialiserade/SerialiseradeVideo.java
@@ -1,6 +1,19 @@
 package GottÅBlandat.Serialiserade;
 
 public class SerialiseradeVideo {
+        //ObjectInputStream
+        //ObjectOutputSteam
+        //Kom ihåg att objektet måste implementa serializable!
+        //if(input instanceof ~String~)
+            //Now safe to cast to String cause already checked if String.
+
+    //med instanceof kan du ge objektet ett namn
+    //if(input instanceof object ~name~)
+        //Nu har objektet ett namn
+
+    //Kom ihåg att skriva objekt för allt.
+    //t.ex "Success", "kvittens", "initiator" för att läsa dem med instanceof till och från servern för att ha koll på vad som händer.
+
 
 
 }
